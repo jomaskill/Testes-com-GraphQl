0768d6f8e787782e9ac0e3ee5800300cf57f0096
jomas <jomasjoao@gmail.com>
2020-01-24 12:27:52 -0300

    adiciona arquivos

---
+++ server.js
@@ -0,0 +1,34 @@
+const { GraphQLServer } = require('graphql-yoga')
+const conexao = require('./infraestrutura/conexao')
+const Tabelas = require('./infraestrutura/database/tabelas')
+const Operacoes = require('./infraestrutura/operations')
+
+
+conexao.connect(erro => {
+  if (erro) {
+    console.log(erro)
+  }
+
+  console.log('conectou no banco')
+
+  Tabelas.init(conexao)
+})
+
+const Clientes = new Operacoes('cliente')
+
+const resolvers = {
+  Query: {
+      status: () => "Servidor rodando!",
+      clientes: () => Clientes.lista()
+  },
+  Mutation: {
+      adicionarCliente: (root, params) => Clientes.adiciona(params)
+  }
+}
+
+const servidor = new GraphQLServer({
+  resolvers,
+  typeDefs: './schema.graphql'
+})
+
+servidor.start(() => console.log('servidor ouvindo'))
